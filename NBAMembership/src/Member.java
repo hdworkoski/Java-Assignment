3b227f834e1fc9c542b90f2e88153d6e5101caf3
Unknown <hdworkoski@gmail.com>
2018-02-22 17:28:28 +1100

    Member class created with variables

---
+++ NBAMembership/src/Member.java
@@ -0,0 +1,12 @@
+/**
+ *
+ * @author hillarydworkoski
+ */
+public class Member
+{
+    protected String ID;
+    protected String firstName;
+    protected String lastName;
+    protected String phone;
+    protected String email;
+}
