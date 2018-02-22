03aa95a370d83016ea0deb10bdd2cd42a5516751
Unknown <hdworkoski@gmail.com>
2018-02-22 17:31:31 +1100

    Constructor, getter, setters created for Member

--- NBAMembership/src/Member.java
+++ NBAMembership/src/Member.java
@@ -2,11 +2,70 @@
  *
  * @author hillarydworkoski
  */
-public class Member
+public abstract class Member
 {
     protected String ID;
     protected String firstName;
     protected String lastName;
     protected String phone;
     protected String email;
+    
+    public Member(String ID, String firstName, String lastName, String phone, String email)
+    {
+        this.ID = ID;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.phone = phone;
+        this.email = email;
+    }
+
+    public String getID()
+    {
+        return ID;
+    }
+
+    public void setID(String ID)
+    {
+        this.ID = ID;
+    }
+
+    public String getFirstName()
+    {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName)
+    {
+        this.firstName = firstName;
+    }
+
+    public String getLastName()
+    {
+        return lastName;
+    }
+
+    public void setLastName(String lastName)
+    {
+        this.lastName = lastName;
+    }
+
+    public String getPhone()
+    {
+        return phone;
+    }
+
+    public void setPhone(String phone)
+    {
+        this.phone = phone;
+    }
+
+    public String getEmail()
+    {
+        return email;
+    }
+
+    public void setEmail(String email)
+    {
+        this.email = email;
+    }
 }
