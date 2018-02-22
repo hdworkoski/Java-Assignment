0d27e206949e21cb03738d45f2eb842afe602138
Unknown <hdworkoski@gmail.com>
2018-02-22 17:53:14 +1100

    Player variables added
    Additional variables for Player class created

--- NBAMembership/src/Player.java
+++ NBAMembership/src/Player.java
@@ -6,15 +6,19 @@ public class Player extends Member
 {
     protected String number;
     protected String college;
-    protected boolean rookie;
-    protected int startYear;
+    protected boolean rookie; //are they a rookie this year?
+    protected int startYear; //year they started in NBA
     protected String position;
     protected String country;
+    protected double ppg; //points per game
+    protected double rpg; //rebounds per game
+    protected int highScore; //highest score ever in one game
     
     public Player
         (String ID, String firstName, String lastName, String phone,
                 String email, String number, String college, boolean rookie,
-                    int startYear, String position, String country)
+                    int startYear, String position, String country, double ppg,
+                        double rpg, int highScore)
         {
             super(ID, firstName, lastName, phone, email);
             this.number = number;
@@ -23,6 +27,9 @@ public class Player extends Member
             this.startYear = startYear;
             this.position = position;
             this.country = country;
+            this.ppg = ppg;
+            this.rpg = rpg;
+            this.highScore = highScore;
         }
 
     public String getNumber()
